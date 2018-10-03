990e787ec93d5ea8ed81d06ee573083716c4811b
blee1170 <brian@brianlee.org>
2018-10-02 22:11:50 -0500

    Adding more town logic

--- cmd/room.go
+++ cmd/room.go
@@ -2,22 +2,44 @@ package buildings
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
+	"time"
 )
 
 //RoomStruct for building rooms
 type RoomStruct struct {
-	X1     int
-	X2     int
-	Y1     int
-	Y2     int
-	Center [2]float64
+	X1 int
+	X2 int
+	Y1 int
+	Y2 int
 }
 
 //LayoutStruct for holding a bunch of rooms
 type LayoutStruct struct {
-	Room []RoomStruct
+	Room   []RoomStruct
+	Blocks [][]int
+	Edges  RoomStruct
+}
+type Point struct {
+	X, Y int
+}
+
+func NewTown(size int, buildings int) *LayoutStruct {
+	rand.Seed(time.Now().UnixNano())
+	block := make([][]int, size)
+	for i := 0; i < size; i++ {
+		block[i] = make([]int, size)
+	}
+	layout := &LayoutStruct{
+		Room:   []RoomStruct{},
+		Blocks: block,
+		Edges:  RoomStruct{X1: 1, Y1: 1, X2: size, Y2: size},
+	}
+	for i := 1; i <= buildings; i++ {
+		layout.AddRoom()
+	}
+
+	return layout
 }
 
 //Intersects Rooms. Make sure they do not over lap.
@@ -47,17 +69,13 @@ func (a *LayoutStruct) AddRoom() {
 	width := randomInt(4, 51)
 	height := randomInt(4, 51)
 	var x1, x2, y1, y2 int
-	//Center
-	var center [2]float64
 
 	//Build the walls
 	x1 = x
 	x2 = x + width
 	y1 = y
 	y2 = y + height
-	center[0] = math.Floor((float64(x1) + float64(x2)) / 2)
-	center[1] = math.Floor((float64(y1) + float64(y2)) / 2)
-	newRoom := RoomStruct{X1: x1, X2: x2, Y1: y1, Y2: y2, Center: center}
+	newRoom := RoomStruct{X1: x1, X2: x2, Y1: y1, Y2: y2}
 
 	doIntersect := false
 	for _, existingRoom := range a.Room {
@@ -105,4 +123,46 @@ func PrintRoom(r *LayoutStruct) {
 		}
 		fmt.Printf("\n%v\n", r.Room[i])
 	}
+
+}
+
+func (t *LayoutStruct) PrintTown() {
+	for _, row := range t.Blocks {
+		for _, col := range row {
+			if col == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
+}
+func (r *RoomStruct) Contains(p Point) bool {
+	return p.X >= r.X1 &&
+		p.Y >= r.Y1 &&
+		p.X <= r.X1+r.X2 &&
+		p.Y <= r.Y1+r.X2
+}
+func (room *LayoutStruct) SetPoint(p Point, cellType int) {
+	if room.Edges.Contains(p) {
+		room.Blocks[p.X][p.Y] = cellType
+	}
+}
+
+func (room *LayoutStruct) setBlock(newBlock *RoomStruct) {
+	for x := 0; x < newBlock.X2; x++ {
+		for y := 0; y < newBlock.Y2; y++ {
+			room.SetPoint(Point{X: x + newBlock.X1, Y: y + newBlock.Y1}, 1)
+		}
+	}
+}
+
+func Contains(arr []int, item int) bool {
+	for _, v := range arr {
+		if v == item {
+			return true
+		}
+	}
+	return false
 }
--- main.go
+++ main.go
@@ -2,25 +2,14 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 
 	"github.com/blee1170/procedural-generation/cmd"
 )
 
 func main() {
 
-	rand.Seed(time.Now().UnixNano())
-	layout := &buildings.LayoutStruct{
-		Room: []buildings.RoomStruct{},
-	}
-	for i := 1; i <= 50; i++ {
-		layout.AddRoom()
-		// layout.Room = room
-		// fmt.Println(layout.Room)
-		// fmt.Printf("Room: %v\nX1: %v, X2: %v, Y1: %v, Y2: %v, Center: %v-%v\n", i, room.X1, room.X2, room.Y1, room.Y2, room.Center[0], room.Center[1])
-
-	}
-	buildings.PrintRoom(layout)
-	fmt.Printf("\nRooms: %v\n", len(layout.Room))
+	town := buildings.NewTown(50, 20)
+	buildings.PrintRoom(town)
+	town.PrintTown()
+	fmt.Printf("\nRooms: %v\n", len(town.Room))
 }
